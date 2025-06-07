75a89412fc03fdd60eb4bc85893daa688c13955c
Evandro Barros <evandrobarrosb98@gmail.com>
2025-06-07 13:32:15 -0300

    update version 1.2

--- auto-blog-sport/src/content-researcher/content-researcher.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ContentResearcherController } from './content-researcher.controller';
-import { ContentResearcherService } from './services/content-researcher.service';
-
-@Module({
-  controllers: [ContentResearcherController],
-  providers: [ContentResearcherService],
-})
-export class ContentResearcherModule {}
